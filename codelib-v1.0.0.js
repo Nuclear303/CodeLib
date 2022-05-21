5b3231f59a289faff6456d4cb6a9df5c99d520e1
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-05-21 08:21:14 +0200

    First stage

---
+++ codelib-v1.0.0.js
@@ -0,0 +1,3 @@
+import * as bubbleSort from "./js/bubbleSort.js";
+
+console.log(bubbleSort([3, 2, 1]));
---
+++ js/bubbleSort.js
@@ -0,0 +1,15 @@
+export default function bubbleSort(array) {
+    var first;
+    var second;
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array.length; j++) {
+            first = array[j];
+            second = array[j + 1];
+            if (first > second) {
+                array[j] = second;
+                array[j + 1] = first;
+            }
+        }
+    }
+    return array;
+}
