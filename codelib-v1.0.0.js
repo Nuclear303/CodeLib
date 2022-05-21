673f71b5dded21b8dffd8a99f2891fafd3f444c0
Nuclear303 <oliwier.kotlowski1@gmail.com>
2022-05-21 20:15:21 +0200

    Edit

--- codelib-v1.0.0.js
+++ codelib-v1.0.0.js
@@ -1,2 +1,20 @@
-import { sorting } from "./js/bubbleSort.js";
+class sorting{
+    constructor(){
+        this.bubbleSort =(array)=> {
+        var first;
+        var second;
+        for (var i = 0; i < array.length; i++) {
+            for (var j = 0; j < array.length; j++) {
+                first = array[j];
+                second = array[j + 1];
+                if (first > second) {
+                    array[j] = second;
+                    array[j + 1] = first;
+                }
+            }
+        }
+        return array;
+        }
+    }
+}
 const Sorting = new sorting();
